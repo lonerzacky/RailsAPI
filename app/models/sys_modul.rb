ac152cef325618e510eff2fa79ecad9c360abb6a
zackwolf <azhar.zakiy@gmail.com>
2019-01-22 14:45:57 +0800

    Add Model for sys_modul table

---
+++ app/models/sys_modul.rb
@@ -0,0 +1,4 @@
+class SysModul < ApplicationRecord
+  self.table_name = 'sys_modul'
+  self.primary_key = 'sysmodul_kode'
+end
---
+++ test/models/sys_modul_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SysModulTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
