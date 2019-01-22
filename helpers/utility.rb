2eae5088f2a97b3567976f2ad07fee3cc04efeff
zackwolf <azhar.zakiy@gmail.com>
2019-01-22 16:02:24 +0800

    utility create digest SHA1

--- helpers/utility.rb
+++ helpers/utility.rb
@@ -5,3 +5,7 @@ def give_response(response_code, response_message, response_data = '')
       response_data: response_data
   }, status: 200
 end
+
+def create_hash(value)
+  Digest::SHA1.hexdigest value
+end
